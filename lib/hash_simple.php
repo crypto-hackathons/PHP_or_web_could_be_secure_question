6cf5bf072f6bb37f27cfcadb0169151bc811f22a
crypto-hackathons <63258586+crypto-hackathons@users.noreply.github.com>
2020-06-16 07:54:17 +0200

    Flgk

--- lib/hash_simple.php
+++ lib/hash_simple.php
@@ -7,24 +7,27 @@ Trait Hash_simple {
         
     use Seed_simple;
     
+    public static $hash_algo = 'sha256';
     public static $hash_prefix;
     
     public static function hash_init(){
         
-        self::$seed_hash_prefix = hash('sha256', self::file_get_contents(self::$seed_grain_file));
+        self::$hash_prefix = hash(self::$hash_algo, self::file_get_contents(self::$seed_grain_file));
     }
     
-    public static function hash(string $data){
+    public static function hash(string $data, $hash_prefix = false){
         
-        return hash('sha256', self::$hash_prefix.$data);
+        if($hash_prefix === false) $hash_prefix = self::$hash_prefix;
+
+        return hash(self::$hash_algo, $hash_prefix.$data);
     }
     
-    public static function hash_array(array $array){
+    public static function hash_array(array $array, $hash_prefix = false){
         
         $array_hashed = new stdClass();
         shuffle($array);
         $array_hashed->to_share = $array;
-        foreach ($array as $k => $v) $array_hashed->to_store[$k] = hash('sha256', self::$hash_prefix.$v);
+        foreach ($array as $k => $v) $array_hashed->to_store[$k] = self::hash($v);
         
         return $array_hashed;
     }
